524e2d0205c7438c46ca1cb652fcc68857d40173
kanta omi <kantarou112239@gmail.com>
2023-07-02 10:22:51 +0900

    my_pageにrecordを反映

--- app/controllers/my_pages_controller.rb
+++ app/controllers/my_pages_controller.rb
@@ -10,17 +10,28 @@ class MyPagesController < ApplicationController
 
   def show
     @user = current_user
-    @savings = calculate_savings(@user.record)
+    @records = current_user.records
+    @savings = calculate_savings(@records)
   end
 
   private
 
   def calculate_savings(records)
-    total_savings = 0
+    total_savings = 0 
 
-    ITEM_PRICES.each do |item, price|
-      total_savings += price if records[item]
+    records.each do |record|
+      @bottle_bring = record.bottle_bring
+      @packed_lunch = record.packed_lunch
+      @alternative_transportation = record.alternative_transportation
+      @no_eating_out = record.no_eating_out
+
+      total_savings += ITEM_PRICES[:bottle_bring] if @bottle_bring
+      total_savings += ITEM_PRICES[:packed_lunch] if @packed_lunch
+      total_savings += ITEM_PRICES[:alternative_transportation] if @alternative_transportation
+      total_savings += ITEM_PRICES[:no_eating_out] if @no_eating_out
+
     end
+
 
     total_savings
   end
