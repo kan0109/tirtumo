1da86158018b830f15df2657c43cd63145bb8535
kanta omi <kantarou112239@gmail.com>
2023-07-16 14:57:58 +0900

    マイページにいいねした投稿反映

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
   has_many :records
   has_one :target
 
