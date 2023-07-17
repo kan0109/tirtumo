683c30108ac08579a06ab1d29a13343516f490e5
kanta omi <kantarou112239@gmail.com>
2023-07-17 22:40:51 +0900

    投稿一覧の表示変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,13 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 65_535 }
+
+  scope :latest, -> { order(created_at: :desc) }  #desc = 降順
+  scope :old, -> { order(created_at: :asc) }  #asc = 昇順
+  scope :most_liked, -> { includes(:liked_users)
+    .sort_by { |x| x.liked_users.includes(:likes).size }.reverse }
 end
