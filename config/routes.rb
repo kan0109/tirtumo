158c6bc67f6f325ab0be8d225d7a9533bb43ae66
kanta omi <kantarou112239@gmail.com>
2023-08-01 14:04:58 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,24 +11,19 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  get 'my_page', to: 'my_pages#show', as: 'my_page'
-  get 'users/index' => 'users#index'
-
-  resources :users, only: %i[new create]
-  resources :password_resets, only: %i[new create edit update]
-  resources :posts do
-    resources :comments, only: %i[create destroy], shallow: true
-  end
-
   get 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
 
   get '/likes', to: 'posts#likes', as: 'post_likes'
 
+  resources :users, only: %i[index new create]
+  resources :password_resets, only: %i[new create edit update]
+  resource :my_page, only: %i[show]
+  resources :posts do
+    resources :comments, only: %i[create destroy], shallow: true
+  end
   resource :profile, only: %i[show edit update]
-
   resources :records, only: %i[index update]
   resources :targets, only: %i[new create]
-
   resources :notifications, only: %i[index]
 end
