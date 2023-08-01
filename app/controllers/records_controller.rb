8a933018cdfcdc1217c28f58f6885be0ee93cd04
kanta omi <kantarou112239@gmail.com>
2023-08-01 14:09:08 +0900

    リスト項目変更

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -5,7 +5,7 @@ class RecordsController < ApplicationController
     bottle_bring: 100,
     packed_lunch: 300,
     alternative_transportation: 200,
-    no_eating_out: 600
+    no_eating_out: 300
   }.freeze
 
   def index
