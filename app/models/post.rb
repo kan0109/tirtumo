6b1a8d78c7854d9ddfd4433433b43a21d7cdf640
kanta omi <kantarou112239@gmail.com>
2023-08-13 19:32:41 +0900

    tag,postモデル作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,18 +5,47 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   has_many :notifications, dependent: :destroy
-  has_many :post_tags, dependent: :destroy
-  has_many :tags, through: :post_tags, dependent: :destroy
+  has_many :association_post_and_tags, dependent: :destroy
+  has_many :tags, through: :association_post_and_tags
+
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 65_535 }
 
-  scope :latest, -> { order(created_at: :desc) }  # desc = 降順
-  scope :old, -> { order(created_at: :asc) } # asc = 昇順
+  scope :latest, -> { order(created_at: :desc) } 
+  scope :old, -> { order(created_at: :asc) }
   scope :most_liked, lambda {
-                       includes(:liked_users)
-                         .sort_by { |x| x.liked_users.includes(:likes).size }.reverse
-                     }
+                      includes(:liked_users)
+                        .sort_by { |x| x.liked_users.includes(:likes).size }.reverse
+                    }
+
+  def self.search(keyword)
+    where("title LIKE ? or content LIKE ?", "%#{sanitize_sql_like(keyword)}%", "%#{sanitize_sql_like(keyword)}%")
+  end  
+
+  def self.sort_and_paginate(params)
+    posts = self.all
+    if params[:tag_id].present?
+      posts = Tag.find(params[:tag_id]).posts
+    end
+
+    if params[:keyword].present?
+      posts = posts.search(params[:keyword])
+    end
+
+    case params[:sort_order]
+    when "latest"
+      posts = posts.sort_by { |post| post.created_at }.reverse
+    when "old"
+      posts = posts.sort_by { |post| post.created_at }
+    when "most_liked"
+      posts = posts.sort_by { |post| -post.liked_users.size }
+    else
+      posts = posts.sort_by { |post| post.created_at }.reverse
+    end
+
+    Kaminari.paginate_array(posts).page(params[:page]).per(10)
+  end
 
   def create_notification_like!(current_user)
     # すでに「いいね」されているか検索
@@ -56,13 +85,5 @@ class Post < ApplicationRecord
     # 自分の投稿に対するコメントの場合は、通知済みとする
     notification.checked = true if notification.visitor_id == notification.visited_id
     notification.save if notification.valid?
-  end
-
-  # def self.ransackable_associations(auth_object = nil)
-  #   ["tag"]
-  # end
-
-  # def self.ransackable_attributes(auth_object = nil)
-  #   ["title", "content"]
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ApplicationRecord
-  has_many :post_tags, dependent: :destroy
-  has_many :posts, through: :post_tags, dependent: :destroy
+
+  has_many :association_post_and_tags, dependent: :destroy
+  has_many :posts, through: :association_post_and_tags
+
+validates :tag_type, presence: true
 end
