20c935a33a07e7b0cc27e8f56ea0c7e12620f79e
kanta omi <kantarou112239@gmail.com>
2023-08-18 20:14:18 +0900

    records _controllerの整理

--- app/controllers/my_pages_controller.rb
+++ app/controllers/my_pages_controller.rb
@@ -1,36 +1,7 @@
 class MyPagesController < ApplicationController
   before_action :require_login
 
-  ITEM_PRICES = {
-    bottle_bring: 100,
-    packed_lunch: 300,
-    alternative_transportation: 200,
-    no_eating_out: 300
-  }.freeze
-
   def show
     @user = current_user
-    @records = current_user.records
-    @savings = calculate_savings(@records)
-  end
-
-  private
-
-  def calculate_savings(records)
-    total_savings = 0
-
-    records.each do |record|
-      @bottle_bring = record.bottle_bring
-      @packed_lunch = record.packed_lunch
-      @alternative_transportation = record.alternative_transportation
-      @no_eating_out = record.no_eating_out
-
-      total_savings += ITEM_PRICES[:bottle_bring] if @bottle_bring
-      total_savings += ITEM_PRICES[:packed_lunch] if @packed_lunch
-      total_savings += ITEM_PRICES[:alternative_transportation] if @alternative_transportation
-      total_savings += ITEM_PRICES[:no_eating_out] if @no_eating_out
-    end
-
-    total_savings
   end
 end
