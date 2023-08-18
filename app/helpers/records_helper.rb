6863297ad95a16bd40134869afff82a534c98fc0
kanta omi <kantarou112239@gmail.com>
2023-08-18 20:16:18 +0900

    ヘルパーの整理

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -1,14 +1,3 @@
 module RecordsHelper
-  def determine_level(level)
-    case level
-    when 1..5
-      '見習い'
-    when 6..15
-      '一人前'
-    when 16..25
-      '名人'
-    else
-      '達人'
-    end
-  end
+  
 end
---
+++ app/models/shared_constants.rb
@@ -0,0 +1,8 @@
+module SharedConstants
+  ITEM_PRICES = {
+    bottle_bring: 100,
+    packed_lunch: 300,
+    alternative_transportation: 200,
+    no_eating_out: 300
+  }.freeze
+end
