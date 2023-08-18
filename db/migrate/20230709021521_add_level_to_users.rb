bc8b8d78dc1d05029928114af75571e8a13db156
kanta omi <kantarou112239@gmail.com>
2023-08-18 20:12:51 +0900

    マイグレーション変更、userテーブルのtotal_savingsとlevelカラム削除

--- db/migrate/20230709021521_add_level_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLevelToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :level, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -103,8 +103,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_12_065416) do
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
     t.string "avatar"
-    t.integer "level"
-    t.integer "total_savings"
     t.integer "target_amount"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
