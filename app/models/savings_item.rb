a2fabc1b9d441d5439de1246bd4c042bceec41b8
kanta omi <kantarou112239@gmail.com>
2023-08-21 16:19:22 +0900

    バリデーション追加

--- app/models/savings_item.rb
+++ app/models/savings_item.rb
@@ -1,6 +1,6 @@
 class SavingsItem < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true
-  validates :amount, presence: true
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :amount, presence: true, numericality: { less_than: 1000 }
 end
