2892b178370ca1715b947c6a2e942171ef951b5c
kanta omi <kantarou112239@gmail.com>
2023-09-01 13:18:26 +0900

    通知機能削除

--- app/controllers/notifications_controller.rb
+++
@@ -1,8 +0,0 @@
-class NotificationsController < ApplicationController
-  def index
-    @notifications = current_user.passive_notifications.page(params[:page]).per(15)
-    @notifications.where(checked: false).each do |notification|
-      notification.update_attribute(:checked, true)
-    end
-  end
-end
