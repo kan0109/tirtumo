335168083dbb5b9fb84e39f4e953d328b7344c67
kanta omi <kantarou112239@gmail.com>
2023-09-01 13:13:32 +0900

    device用マイグレーションファイル作成

---
+++ db/migrate/20230831010819_add_column_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :name, :string, null:false
+    add_column :users, :avatar, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_063706) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_31_010819) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,16 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_063706) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_association_post_and_tags_on_post_id"
     t.index ["tag_id"], name: "index_association_post_and_tags_on_tag_id"
-  end
-
-  create_table "authentications", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "provider", null: false
-    t.string "uid", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
-    t.index ["user_id"], name: "index_authentications_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -113,10 +103,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_063706) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name", null: false
     t.string "avatar"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "association_post_and_tags", "posts"
